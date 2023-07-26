ac5b652a787baeca40bbe663621d493ebfdcac2d
Yuetian Chen <j.a.m_01@outlook.com>
2023-07-26 17:22:38 -0400

    Add arg parse

--- __main__.py
+++ __main__.py
@@ -1,11 +1,10 @@
-
-
+import argparse
 from torch.utils.data import DataLoader
 
 from transformer import *
-from trainer import AutoregressiveTrainer
+from autoregressivetrainer import AutoregressiveTrainer
+from berttrainer import BertTrainer
 from dataset import TextDataset
-
 
 """
 Supported datasets:
@@ -15,34 +14,35 @@ scientific_papers
 
 """
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seq_len", default=512, type=int)
+    parser.add_argument("--vocab_size", default=30522, type=int)
+    parser.add_argument("--n_layers", default=4, type=int)
+    parser.add_argument("--d_model", default=768, type=int)
+    parser.add_argument("--n_head", default=8, type=int)
+    parser.add_argument("--p", default=0.1, type=float)
+    parser.add_argument("--lr", default=4e-5, type=float)
+    parser.add_argument("--batch_size", default=32, type=int)
+    parser.add_argument("--burnin", default=0, type=int)
+    parser.add_argument("--rollout", default=5, type=int)
+    parser.add_argument("--warmup_steps", default=100, type=int)
+    parser.add_argument("--device", default="cuda:0", type=str)
+    parser.add_argument("--cache_dir", default="./cache/datasets", type=str)
 
-def main(
-    dataset="scientific_papers",
-    seq_len=512,
-    vocab_size=30522,
-    n_layers=4,
-    d_model=768,
-    n_head=8,
-    p=0.1,
-    lr=4e-5,
-    batch_size=32,
-    burnin=0,
-    rollout=5,
-    device="cuda",
-    cache_dir="/media/yh04/New Volume/datasets"
-):
+    args = parser.parse_args()
 
     lm = AutoregressiveLM(
         cls=RecurrentMemoryTransformer,
-        vocab_size=vocab_size,
-        max_len=seq_len,
-        n_layers=n_layers,
-        d_model=d_model,
-        n_head=n_head,
-        p=p,
-        device=device,
+        vocab_size=args.vocab_size,
+        max_len=args.seq_len,
+        n_layers=args.n_layers,
+        d_model=args.d_model,
+        n_head=args.n_head,
+        p=args.p,
+        device=args.device,
         w=128,
-        bsz=batch_size,
+        bsz=args.batch_size,
         topk=1,
         num_tokens=128,
         mem_tokens=64,
@@ -51,26 +51,27 @@ def main(
 
     dataloader = DataLoader(
         TextDataset(
-            name=dataset,
-            cache_dir=cache_dir,
+            name="scientific_papers",
+            cache_dir=args.cache_dir,
             split="train[:20000]",
-            seq_len=seq_len,
-            block_len=rollout,
-            device=device,
+            seq_len=args.seq_len,
+            block_len=args.rollout,
+            device=args.device,
             sep_padding=True
         ),
-        batch_size=batch_size,
+        batch_size=args.batch_size,
     )
 
     trainer = AutoregressiveTrainer(
         model=lm,
         dataloader=dataloader,
-        lr=lr,
-        batch_size=batch_size,
-        seqlen=seq_len,
-        burnin=burnin,
-        rollout=rollout,
-        device=device
+        lr=args.lr,
+        batch_size=args.batch_size,
+        seqlen=args.seq_len,
+        burnin=args.burnin,
+        rollout=args.rollout,
+        warmup_steps=args.warmup_steps,
+        device=args.device
     )
     print("Starting training run...")
 
@@ -81,4 +82,3 @@ def main(
 
 if __name__ == "__main__":
     main()
-
